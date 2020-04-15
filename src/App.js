ef4ef9fb4207c7567d4710b9c8bcc2f3a98ff1fc
Cooper Higgins <cooperhiggins@gmail.com>
2020-04-15 05:22:37 -0400

    created components directory finished app.css styles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-          <div className="col-6">
-              column number one
-          </div>
-          <div className="col-6">
-              <span>
-                <i className="fa fa-home" />
-              </span>
-          </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <h3>Hello from react app</h3>
+    </React.Fragment>
   );
 }
 
