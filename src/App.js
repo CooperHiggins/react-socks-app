213fb66a4afc0d05ba7a41c750956b12a75336c7
Cooper Higgins <cooperhiggins@gmail.com>
2020-04-15 06:09:08 -0400

    created components directory created component files cart, default, details, navbar, product, productlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
     <React.Fragment>
+      <Navbar />
       <h3>Hello from react app</h3>
+      <ProductList />
+      <Details />
+      <Cart />
+      <Default />
     </React.Fragment>
+
   );
 }
 
